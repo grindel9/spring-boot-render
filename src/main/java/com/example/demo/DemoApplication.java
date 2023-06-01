66a00fb4bf6bb538fafe7251379d1c79dc62053d
Václav Lokaj <vaclav.lokaj-extern@renault.cz>
2023-06-02 01:31:53 +0200

    refactor GET & POST

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,37 +1,40 @@
 package com.example.demo;
 
+import java.util.List;
+
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-	@Autowired
-	UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@GetMapping("count-users")
-	public String getCountUsers(){
-		long numberUsers = userRepository.count();
-		return "Number of users = "+numberUsers;
-	}
+    @GetMapping("user")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
 
-	@GetMapping("add-user")
-	public String addUser(){
-		User user = new User();
-		user.setFirstName("Render");
-		user.setLastName("Spring Boot");
-		userRepository.save(user);
-		return "User added successfully";
-	}
+    @PostMapping("user")
+    public ResponseEntity<Void> addUser(@RequestBody User user) {
+        userRepository.save(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
